b89d86251a2b0a4c4b56f7beefe8120f4bbdc868
akhmed0606 <analgiev57gmail.com>
2022-01-31 21:50:45 -0500

    created js file & selected all elements needed

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const portfolioApp = {};
+
+portfolioApp.form = document.querySelector('.contactForm');
+portfolioApp.projectButton = document.querySelectorAll('.projectButton')
+portfolioApp.closeButton = document.querySelectorAll('.fa-times')
+portfolioApp.closeMenu = document.querySelector('.menuClose')
+portfolioApp.openMenu = document.querySelector('.menuOpen')
+portfolioApp.mobileMenu = document.querySelector('.navBarMobile')      
